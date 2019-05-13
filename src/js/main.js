4a2c1e6f0db81f0d1c0363c2a11dbdaede08f87c
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-13 08:17:00 -0400

    Rough out event handling logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+const __EVENTS__ = {
+  REFINEMENT_SELECTED: 'refinementSelected',
+  REFINEMENT_DESELECTED: 'refinementDeselected',
+};
+
 const __DATA__ = [
   {
     refinements: [
@@ -13,6 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
   class Navigation extends HTMLElement {
     constructor() {
       super();
+
+      // Bind.
+      this.handleRefinementClick = this.handleRefinementClick.bind(this);
 
       this.data = __DATA__;
       const root = this.attachShadow({ mode: 'open' });
@@ -34,6 +42,9 @@ window.addEventListener('DOMContentLoaded', () => {
           checkBoxElem.setAttribute('type', 'checkbox');
           checkBoxElem.id = id;
 
+          // Bind event handlers.
+          checkBoxElem.addEventListener('click', this.handleRefinementClick);
+
           // Assemble document fragment.
           listItemElem.appendChild(checkBoxElem);
           listItemElem.appendChild(labelElem);
@@ -45,7 +56,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
       root.appendChild(elem);
     }
+
+    handleRefinementClick(e) {
+      e.target.checked
+        ? this.emit(__EVENTS__.REFINEMENT_SELECTED)
+        : this.emit(__EVENTS__.REFINEMENT_DESELECTED);
+    }
+
+    emit(eventName, payload) {
+      window.dispatchEvent(new CustomEvent(eventName, { detail: payload }));
+    }
   }
 
   window.customElements.define('my-navigation', Navigation);
+
+  window.addEventListener(__EVENTS__.REFINEMENT_SELECTED, (e) => {
+    console.log('__ RECEIVED `REFINEMENT_SELECTED` EVENT', e);
+  });
+
+  window.addEventListener(__EVENTS__.REFINEMENT_DESELECTED, (e) => {
+    console.log('__ RECEIVED `REFINEMENT_DESELECTED` EVENT', e);
+  });
 });
