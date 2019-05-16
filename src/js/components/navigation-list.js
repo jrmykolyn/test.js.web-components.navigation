38f4db18c990c32ec1490ff0731867fcc78845c3
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-16 14:56:17 -0400

    Make NavigationList slottable

--- src/js/components/navigation-list.js
+++ src/js/components/navigation-list.js
@@ -13,6 +13,7 @@
         padding: 0;
       }
     </style>
+    <slot name="content"></slot>
   `;
 
   window.__COMPONENTS__ = window.__COMPONENTS__ || {};
@@ -22,20 +23,21 @@
       super();
 
       this.root = this.attachShadow({ mode: 'open' });
-      this.node = document.createElement('ul');
       this.root.appendChild(template.content.cloneNode(true));
-      this.root.appendChild(this.node);
+      this.slots = this.getSlots();
+      this.node = document.createElement('ul');
+      this.slots.content.appendChild(this.node);
     }
 
     render() {
-      this.node.innerHTML = '';
+      this.slots.content.innerHTML = '';
 
       this.refinements.forEach(({ name, value }) => {
         const listItemElem = document.createElement('my-navigation-list-item');
         listItemElem.setAttribute('refinement-name', name);
         listItemElem.setAttribute('refinement-value', value);
 
-        this.node.appendChild(listItemElem);
+        this.slots.content.appendChild(listItemElem);
       });
     }
   }
